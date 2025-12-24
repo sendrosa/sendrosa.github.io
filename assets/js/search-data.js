1de8c518e81ecdcce78fed72648740cacc46851d
sendrosa <sendrosaster@gmail.com>
2025-12-24 14:10:44 +0000

    Deploying to gh-pages from @ sendrosa/sendrosa.github.io@655b0b5e40d980674d2f1e5b055233f07eb53a9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,29 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/plotly/";
-          
-        },
-      },{id: "post-a-post-with-image-galleries",
-        
-          title: "a post with image galleries",
-        
-        description: "this is what included image galleries could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/photo-gallery/";
-          
-        },
-      },{id: "books-the-godfather",
+        },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
